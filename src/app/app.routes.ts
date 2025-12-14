fce871eb5d85bafe7895391ca8237974deb296c0
viktor <v.huanca1999@gmai.com>
2025-12-14 16:04:13 -0300

    buttons y headers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Dashboard } from './pages/dashboard/dashboard';
 import { Cards } from './pages/cards/cards';
 import { Headers } from './pages/headers/headers';
 import { Alerts } from './pages/alerts/alerts';
+import { Buttons } from './pages/buttons/buttons';
 
 export const routes: Routes = [
     { path: '', component: Dashboard },
     { path: 'dashboard', component: Dashboard },
     { path: 'alerts', component: Alerts },
+    { path: 'buttons', component: Buttons },
     { path: 'cards', component: Cards },
     { path: 'headers', component: Headers },
 ];
---
+++ src/app/pages/buttons/buttons.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons',
+  standalone: true,
+  imports: [],
+  templateUrl: './buttons.html',
+  styleUrl: './buttons.css',
+})
+export class Buttons {
+
+}
